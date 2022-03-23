a4a4791ca23670958675b47b0ef4289840f8862c
RIX <mc.therash@gmail.com>
2022-03-23 18:23:12 +0100

    implemented: Board.__str__ implemented: check_input >implemented: check_char, check_word, check_status

--- logic.py
+++ logic.py
@@ -26,3 +26,44 @@ class Board:
                 self.current += " "
             else:
                 self.current += "_"
+
+    def __str__(self):
+        result = str(self.solution) + "\n" + str(self.current) + "\n" + str(self.wrong_guesses) + "\n" + str(self.status)
+        return result
+
+
+def check_word(word, board):
+    if word == board.solution:
+        board.current = board.solution
+        return True
+    if word not in board.wrong_guesses:
+        board.wrong_guesses.append(word)
+    return False
+
+
+def check_char(c, board):
+    if c not in board.solution:
+        if c not in board.wrong_guesses:
+            board.wrong_guesses.append(c)
+        return False
+    for i in range(len(board.solution)):
+        if board.solution[i] == c:
+            board.current = board.current[:i] + c + board.current[i + 1:]
+    return True
+
+
+def check_status(board):
+    if len(board.wrong_guesses) >= 12:
+        board.status = StatusType.lose
+    elif board.current == board.solution:
+        board.status = StatusType.win
+
+
+def check_input(guess, board):
+    if len(guess) != 1:
+        result = check_word(guess, board)
+    else:
+        result = check_char(guess, board)
+    check_status(board)
+    return result
+
