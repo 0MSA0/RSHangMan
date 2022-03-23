1c6f598dc4974b58d59879b8d451fd9079116f1a
MSA <basti.aquila@gmail.com>
2022-03-23 20:08:45 +0100

    Added base functionality. To Do: Nice graphics

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import logging
 import discord
-#import logic.py
+import logic
 import json
 from discord.ext import commands, tasks
+
 
 
 
@@ -15,16 +16,61 @@ handler = logging.FileHandler(
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-def get_config():
-    with open("config.json", "r", encoding='utf-8') as f:
-        config = json.load(f)
-        global token
-        token = config["token"]
-        global prefix
-        prefix = config["prefix"]
-    print("Successfully loaded config.")
+with open("config.json", "r", encoding='utf-8') as f:
+    config = json.load(f)
+    bot_token = config["token"]
+    bot_prefix = config["prefix"]
+    bot_description = config["description"]
+print("Successfully loaded config.")
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(bot_prefix),
+                        description=bot_description)
+
+game = None
+
+@bot.command(aliases=["ng"])
+async def new_game(ctx):
+    global game
+    game = logic.Board()
+    print(game)
+    await ctx.send(str(game))
+
+@bot.command()
+async def guess(ctx, *args):
+    """
+    Guess a character, word or a sentence.
+    Usage: !guess ...
+    """
+    output_str = "No game is running"
+    if game is not None and game.status == logic.StatusType.running:
+        if len(args) == 0:
+            output_str = "Please guess a character or a word!"
+        else:
+            guess_str = ''
+            for wordnr in range(len(args) - 1):
+                guess_str += args[wordnr] + ' '
+            guess_str += args[len(args) - 1]
+            is_in_solution = logic.check_input(guess_str, game)
+            if is_in_solution == True:
+                output_str = 'Oui'
+                if game.status == logic.StatusType.win:
+                    output_str = "Big oui"
+            else:
+                output_str = 'Nö'
+                if game.status == logic.StatusType.lose:
+                    output_str = "Big nöö"    
+    await ctx.send(output_str)
+
+
+@bot.command(aliases=["sh"])
+@commands.is_owner()
+async def shutdown(ctx):
+    await ctx.send("Bye bye :D")
+    await bot.close()
+    print("Bot shutting down")
+   
 
 if __name__ == "__main__":
-    get_config()
     print("I am alive!")
+    bot.run(bot_token)
+    
     
